a448af821797644d1a867b6c2281644d7b622de6
Mohi-th <mohithmohith880@gmail.com>
2026-08-06 16:00:25 +0530

    changed api url settings for docker and hosting

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -6,7 +6,7 @@ import axios from "axios";
  * In development, Vite proxy handles it (configured in vite.config.js).
  */
 const api = axios.create({
-  baseURL: "/api",
+  baseURL:import.meta.env.VITE_API_URL|| "/api",
   headers: {
     "Content-Type": "application/json",
   },
